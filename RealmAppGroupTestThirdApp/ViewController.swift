cca55f541a392a8bfeef947ae7032c4734494751
Paul Uhn <paul.uhn@motorolasolutions.com>
2017-03-28 13:29:17 -0500

    Sync

--- RealmAppGroupTestThirdApp/ViewController.swift
+++ RealmAppGroupTestThirdApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmAppGroupTestFramework
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("sync...")
+        SyncClient.sync {
+            print("sync done")
+        }
+    }
 
 }
 
